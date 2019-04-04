0a6cd974e7453a8bc832e3a161616fae915db5b6
Gamal Elsawy <gamalelsawy11@gmail.com>
2019-04-04 02:47:50 +0200

    Task is done

--- Pro/src/pro/Pro.java
+++ Pro/src/pro/Pro.java
@@ -1,21 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package pro;
-
-/**
- *
- * @author Gamal Elsawy
- */
 public class Pro {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
+        // Starting Main
         System.out.println("Hello Monopoly !!!");
+        
     }
     
 }
