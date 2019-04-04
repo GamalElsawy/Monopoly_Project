59b824b7f47b6f792bcee78de1ac948be8ffbb3a
Gamal Elsawy <gamalelsawy11@gmail.com>
2019-04-04 02:38:35 +0200

    donee

--- Pro/src/pro/Pro.java
+++ Pro/src/pro/Pro.java
@@ -15,7 +15,7 @@ public class Pro {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("cx");
+        System.out.println("Hello Monopoly !!!");
     }
     
 }
