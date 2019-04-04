bcb60c2a3f2b5f833ea0062b3837df9e83401177
Gamal Elsawy <gamalelsawy11@gmail.com>
2019-04-04 03:03:01 +0200

    Last Update

--- Pro/src/pro/Pro.java
+++ Pro/src/pro/Pro.java
@@ -3,6 +3,7 @@ public class Pro {
     public static void main(String[] args) {
         // Starting Main
         System.out.println("Hello Monopoly !!!");
+        String Stat = "GitHup is released"; 
         
     }
     
