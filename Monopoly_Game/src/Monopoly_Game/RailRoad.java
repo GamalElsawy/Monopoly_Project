9b7cf57620faa17c8a69c58d04f094900a25fc4b
Hisham Gamal <hishamgamal5@gmail.com>
2019-04-11 18:32:53 +0200

    Adding new data fields into RailRoad class

--- Monopoly_Game/src/Monopoly_Game/RailRoad.java
+++ Monopoly_Game/src/Monopoly_Game/RailRoad.java
@@ -6,9 +6,13 @@ import java.awt.event.*;
 
 public class RailRoad extends Field{
     int[] Rent;
-    public RailRoad(String n,int x,int y,String PathImage) {
+    int Mort, Price;
+    public RailRoad(String n,int x,int y,String PathImage, int[] rent, int mort, int price) {
         super(n,x,y,PathImage);
         Rent = new int[5];
+        System.arraycopy(rent, 1, Rent, 1, 4);
+        Mort = mort;
+        Price = price;
     }
     int payRent(int noRailroads){
         return Rent[noRailroads];
