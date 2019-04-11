6e80a476aeec666e77894424fabdc9265b0c5ed9
Hisham Gamal <hishamgamal5@gmail.com>
2019-04-11 19:06:24 +0200

    Making the data in RailRoad class static + making a text file with for the money stuff for railroads

--- Monopoly_Game/src/Monopoly_Game/RailRoad.java
+++ Monopoly_Game/src/Monopoly_Game/RailRoad.java
@@ -5,14 +5,10 @@ import java.awt.*;
 import java.awt.event.*;
 
 public class RailRoad extends Field{
-    int[] Rent;
-    int Mort, Price;
-    public RailRoad(String n,int x,int y,String PathImage, int[] rent, int mort, int price) {
+    static int[] Rent = {0, 25, 50, 100, 200};
+    static int Mort = 100, Price = 200;
+    public RailRoad(String n,int x,int y,String PathImage) {
         super(n,x,y,PathImage);
-        Rent = new int[5];
-        System.arraycopy(rent, 1, Rent, 1, 4);
-        Mort = mort;
-        Price = price;
     }
     int payRent(int noRailroads){
         return Rent[noRailroads];
